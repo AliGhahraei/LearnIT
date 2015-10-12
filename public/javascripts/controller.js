f8f23d8d67dd9e2b13092a023bebc853f2712258
Heloel Hernández Santos <heloelhdez@htomail.com>
2015-10-12 00:01:21 -0500

    comentarios
    Comments para controller.js

--- public/javascripts/controller.js
+++ public/javascripts/controller.js
@@ -1,5 +1,15 @@
+/**Asigna a una variable el modulo de angular
+En este caso es el body
+*/
 var app = angular.module('mainApp',['ngRoute']);
-
+/*
+Esta es la implementacion de las rutas
+lo que hace es simplemente el direccionamiento de las
+paginas.
+En el caso de dashboar, revisa que se tenga iniciada la sesion
+Route lo manera nternamente angular, por eso nosotros no lo pasamos
+solamente implementamos los casos.
+*/
 app.config(function($routeProvider) {
 	$routeProvider
 	.when('/',{
@@ -19,7 +29,25 @@ app.config(function($routeProvider) {
 		redirectTo: '/'
 	})
 })
+/*
+En este caso implementacion la funcionalidad del login
+Tenemos $scope para describrirlo mejor copio esto:
 
+$scope is the link between your view and controller. 
+Any property that the html evaluates is evaluated on a particular scope. 
+For eg.  When you write {{name}}, Angular searches for name property in the corresponding scope. 
+
+Scope allows propagation of changes in your models (i.e.  any property of scope eg $scope. name) 
+to your views. This is called 2 way binding. 
+
+Por lo anterior utilizamos el evento submit y corremos la funcion
+por medio de scope agarramos las varibles de user y password que son asignadas
+unicamente para ser mas transparentes.
+$location muestra y asigna ubicacion, por eso lo pedimos.
+
+$rootscope es una variable gobal y desde ella podemos acceder a username y password
+desde donde queramos. Asi como loggedin.
+*/
 app.controller('loginCtrl', function($scope, $location, $rootScope){
 	$scope.submit = function() {
 		var uname = $scope.username; //$rootScope es una variable global
