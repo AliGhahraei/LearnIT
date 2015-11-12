99f55b9682fbf28cb23e8d7eb53e6c68524a6ea7
AliGhahraei <aligf94@gmail.com>
2015-11-12 12:42:57 -0600

    El registro ya sirve por completo. Denle.

--- registro/views.py
+++ registro/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from django import forms
 from django.forms.widgets import PasswordInput
+from django.utils.safestring import mark_safe
 
 from django.forms.models import ModelForm 
 from principal.models import Usuario
@@ -9,11 +10,18 @@ from django.contrib.auth.models import User
 
 paginaPrincipal='/'
 
+def requerido(stringOriginal):
+    return mark_safe(('<span class=error>*</span> '+stringOriginal))
+
 class FormaPerfil(ModelForm):
     class Meta:
         model = Usuario
         exclude = ['formPerfil']
         fields = ['carrera']
+        labels = {
+            'carrera': requerido('Major'),
+        }
+    
 class FormaUsuario(ModelForm):
     class Meta:
         model = User
@@ -22,7 +30,11 @@ class FormaUsuario(ModelForm):
         widgets = {
             'password': PasswordInput(), 
         }
-
+        labels = {
+            'username': requerido('Username'),
+            'password': requerido('Password'),
+        }
+    
 def registra(request):
     if request.user.is_authenticated():
         return HttpResponseRedirect(paginaPrincipal)
