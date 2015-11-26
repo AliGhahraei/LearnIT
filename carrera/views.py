c2cb018648600cd8c9eac0e90678554e25cd2309
Ricardo Ricardez <ritirial@gmail.com>
2015-11-26 15:27:56 -0600

    cambios menores en verTemas y verTema

--- carrera/views.py
+++ carrera/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
 
 from principal.models import Usuario, Materia, Carrera, Materias_Carrera
 from django.contrib.auth.models import User
 
 # Create your views here.
+@login_required
 def verCarrera(request):
     miCarrera = request.user.usuario.carrera
     miUsername = request.user.username
