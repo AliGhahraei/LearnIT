5c6f7a5706f59138aef02fc1e54e7509bae0fdb0
Ricardo Ricardez <ritirial@gmail.com>
2015-11-26 16:54:21 -0600

    agregado el crearMateria, parcialmente, no envia post

--- materia/urls.py
+++ materia/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-url(r'^verTemas/(?P<idMateria>[0-9]+)/$',views.verTemas,name='verTemas'),
+	url(r'^verTemas/(?P<idMateria>[0-9]+)/$',views.verTemas,name='verTemas'),
+	url(r'^crearMateria/$',views.crearMateria,name='crearMateria'),
 ]
--- materia/views.py
+++ materia/views.py
@@ -1,9 +1,37 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django import forms
 from django.contrib.auth.decorators import login_required
-from principal.models import Usuario, Materia, Tema, Temas_Materia
-from django.contrib.auth.models import User
-# Create your views here.
+from django.utils.safestring import mark_safe
+from django.forms.formsets import BaseFormSet
 
+from django.forms.models import ModelForm
+from principal.models import Materia, Carrera, Materias_Carrera,Temas_Materia, Tema, Usuario
+from django.contrib.auth.models import User
+
+paginaPrincipal='/'
+
+def requerido(stringOriginal):
+	return mark_safe(('<span class=error>*</span> '+stringOriginal))
+
+class FormSetRequerido(BaseFormSet):
+	def __init__(self, *args, **kwargs):
+		super(FormSetRequerido, self).__init__(*args, **kwargs)
+		for form in self.forms:
+			form.empty_permitted = False
+
+class FormaMateria(ModelForm):
+
+	class Meta:
+		model=Materia
+		fields=['materia']
+		labels = {
+			'materia': requerido('Name')
+		}
+
+	def __init__(self, *args, **kwargs):
+		super(FormaMateria, self).__init__(*args, **kwargs)
+		self.fields['materia'].requred = True
 
 @login_required
 def verTemas(request, idMateria):
@@ -15,3 +43,20 @@ def verTemas(request, idMateria):
 	'materia' : miMateria,
 	'nMateria' : nombreMateria,
 	})
+
+@login_required
+def crearMateria(request):        
+    if request.method == 'POST':
+        if 'submit' in request.POST:
+            formMateria = FormaMateria(request.POST, prefix='materia')
+
+            if formMateria.is_valid():
+                materia = formMateria.save()
+
+                return HttpResponseRedirect(paginaPrincipal)               
+    else:
+        formMateria = FormaMateria(prefix='materia')
+
+    return render(request, 'materia/crearMateria.html', {
+        'formMateria': formMateria,
+    })
