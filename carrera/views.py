678955a0f535b2a14003c2e669148fe196ecf5bd
Ricardo Ricardez <ritirial@gmail.com>
2015-11-26 12:30:38 -0600

    Cambiado verCarrera por VerPerfil, y verMaterias pasa al index

--- carrera/views.py
+++ carrera/views.py
@@ -1,43 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-from principal.models import Materia, Carrera, Materias_Carrera
+from principal.models import Usuario, Materia, Carrera, Materias_Carrera
+from django.contrib.auth.models import User
 
 # Create your views here.
-def verMaterias(request):
-    miCarrera = request.user.usuario.carrera
-    matsCarrera = Materias_Carrera.objects.filter(carrera=miCarrera)
-    lista_materias = []
-    for relacion in matsCarrera:
-        lista_materias.append(Materia.objects.get(materia=relacion.materia))
-
-    return render(request, 'carrera/verMaterias.html',{
-    	'lista_materias' : lista_materias,
-    	'carrera' : miCarrera,
-    })
-
 def verCarrera(request):
-#    miUsername = request.user.usuario.username
-#    miNombre = request.user.usuario.first_name
-#	miApellido = request.user.usuario.last_name
     miCarrera = request.user.usuario.carrera
+    miUsername = request.user.username
+    miNombre = request.user.first_name
+    miApellido = request.user.last_name
 
-    return render(request, 'carrera/verCarrera.html',{
-#    	'usuario' : miUsername,
-#    	'nombres' : miNombre,
-#    	'apellidos' : miApellido,
-    	'carrera' : miCarrera,
+    return render( request, 'carrera/verCarrera.html', {
+    	'usuario' : miUsername,
+    	'nombres' : miNombre,
+    	'apellidos' : miApellido,
+    	'carrera' : miCarrera
     })
-
-#def verTemas(request):
-#    miCarrera = request.user.usuario.carrera
-#    matsCarrera = Materias_Carrera.objects.filter(carrera=miCarrera)
-#    lista_materias = []
-#    lista_temas = []
-#    for relacion in matsCarrera:
-#        lista_materias.append(Materia.objects.get(materia=relacion.materia))
-#    for rel2 in 
-#    return render(request, 'carrera/verMaterias.html',{
-#    	'lista_materias' : lista_materias,
-#    	'carrera' : miCarrera,
-#    })
--- principal/views.py
+++ principal/views.py
@@ -1,6 +1,26 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from principal.models import Usuario, Materia, Carrera, Materias_Carrera
+from django.contrib.auth.models import User
 
 @login_required
 def index(request):
-    return render(request, 'principal/index.html')
+    miCarrera = request.user.usuario.carrera
+    miUsername = request.user.username
+    miCorreo = request.user.email
+    miNombre = request.user.first_name
+    miApellido = request.user.last_name
+
+    matsCarrera = Materias_Carrera.objects.filter(carrera=miCarrera)
+    lista_materias = []
+    for relacion in matsCarrera:
+        lista_materias.append(Materia.objects.get(materia=relacion.materia))
+        
+    return render( request, 'principal/index.html', {
+    	'usuario' : miUsername,
+    	'nombres' : miNombre,
+    	'correo' : miCorreo,
+    	'apellidos' : miApellido,
+    	'carrera' : miCarrera,
+    	'lista_materias' : lista_materias
+    })
