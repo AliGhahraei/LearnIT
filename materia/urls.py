0d7d8dbf6bf8b26ccb59a20789805bb5f8d48016
Heloel Hdez <heloelhdez@hotmail.com>
2015-11-26 18:08:12 -0500

    Ya sirve el ver lista de temas

--- materia/urls.py
+++ materia/urls.py
@@ -1,7 +1,6 @@
 from django.conf.urls import url
-
 from . import views
 
 urlpatterns = [
-
+url(r'^verTemas/(?P<idMateria>[0-9]+)/$',views.verTemas,name='verTemas'),
 ]
--- materia/views.py
+++ materia/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
-
+from django.contrib.auth.decorators import login_required
+from principal.models import Usuario, Materia, Tema, Temas_Materia
+from django.contrib.auth.models import User
 # Create your views here.
+
+
+@login_required
+def verTemas(request, idMateria):
+	miMateria = Materia.objects.filter(pk=idMateria)
+	nombreMateria = miMateria[0]
+	listTemas = Temas_Materia.objects.filter(materia=miMateria)
+	return render(request, 'materia/verTemas.html',{
+	'listTemas': listTemas,
+	'materia' : miMateria,
+	'nMateria' : nombreMateria,
+	})
